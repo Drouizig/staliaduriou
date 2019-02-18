9035d7de5e0799d24d7e50d3c94435ab42bd8558
Gwenn Meynier <tornoz@laposte.net>
2019-02-18 09:13:16 +0100

    add download script

---
+++ download.sh
@@ -0,0 +1,5 @@
+wget "https://download.mozilla.org/?product=firefox-latest-ssl&os=win64&lang=br" -O firefox_win64.exe
+wget "https://download.mozilla.org/?product=firefox-latest-ssl&os=win&lang=br" -O firefox_win32.exe
+wget "https://download.mozilla.org/?product=firefox-latest-ssl&os=osx&lang=br" -O firefox_mac.dmg
+wget "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=br" -O firefox_linux64.tar.bz2
+wget "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux&lang=br" -O firefox_linux32.tar.bz2
