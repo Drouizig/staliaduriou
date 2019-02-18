07b654e6428fae2391f3afb94ed1059897805e2c
tornoz <tornoz@gozmail.bzh>
2019-02-18 10:06:13 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,5 +1,10 @@
-wget "https://download.mozilla.org/?product=firefox-latest-ssl&os=win64&lang=br" -O firefox_win64.exe
-wget "https://download.mozilla.org/?product=firefox-latest-ssl&os=win&lang=br" -O firefox_win32.exe
-wget "https://download.mozilla.org/?product=firefox-latest-ssl&os=osx&lang=br" -O firefox_mac.dmg
-wget "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=br" -O firefox_linux64.tar.bz2
-wget "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux&lang=br" -O firefox_linux32.tar.bz2
+rm -rf builds
+mkdir builds
+wget "https://download.mozilla.org/?product=firefox-latest-ssl&os=win64&lang=br" -O builds/firefox_win64.exe
+wget "https://download.mozilla.org/?product=firefox-latest-ssl&os=win&lang=br" -O builds/firefox_win32.exe
+wget "https://download.mozilla.org/?product=firefox-latest-ssl&os=osx&lang=br" -O builds/firefox_mac.dmg
+wget "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=br" -O builds/firefox_linux64.tar.bz2
+wget "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux&lang=br" -O builds/firefox_linux32.tar.bz2
+wget "https://www.libreoffice.org/donate/dl/win-x86_64/6.2.0/br/LibreOffice_6.2.0_Win_x64.msi" -O builds/libreoffice_win64.msi
+wget "https://www.libreoffice.org/donate/dl/win-x86/6.2.0/br/LibreOffice_6.2.0_Win_x86.msi" -O builds/libreoffice_win32.msi
+wget "https://www.libreoffice.org/donate/dl/mac-x86_64/6.2.0/br/LibreOffice_6.2.0_MacOS_x86-64.dmg" -O builds/libreoffice_mac.dmg
