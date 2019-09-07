b2a66ede8bf8db19a0ca740aea43e4845d0eb7ae
Gwenn <tornoz@laposte.net>
2019-09-07 16:07:29 +0200

    bump version

--- download_builds.sh
+++ download_builds.sh
@@ -9,21 +9,21 @@ wget "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=b
 wget "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux&lang=br" -O static/builds/firefox_linux32.tar.bz2
 
 #Thunderbird
-wget "https://download.mozilla.org/?product=thunderbird-60.5.1-SSL&os=win&lang=br" -O static/builds/thunderbird_win.exe
-wget "https://download.mozilla.org/?product=thunderbird-60.5.1-SSL&os=osx&lang=br" -O static/builds/thunderbird_mac.dmg
+wget "https://download.mozilla.org/?product=thunderbird-68.0-SSL&os=win&lang=br" -O static/builds/thunderbird_win.exe
+wget "https://download.mozilla.org/?product=thunderbird-68.0-SSL&os=osx&lang=br" -O static/builds/thunderbird_mac.dmg
 
 # Libreoffice
-wget "http://mirror.in2p3.fr/ftp/tdf/libreoffice/stable/6.2.0/win/x86_64/LibreOffice_6.2.0_Win_x64.msi" -O static/builds/libreoffice_win64.msi
-wget "http://mirror6.layerjet.com/tdf/libreoffice/stable/6.2.0/win/x86/LibreOffice_6.2.0_Win_x86.msi" -O static/builds/libreoffice_win32.msi
-wget "https://mirror.ibcp.fr/pub/tdf/libreoffice/stable/6.2.0/mac/x86_64/LibreOffice_6.2.0_MacOS_x86-64.dmg" -O static/builds/libreoffice_mac.dmg
+wget "https://download.documentfoundation.org/libreoffice/stable/6.3.1/win/x86_64/LibreOffice_6.3.1_Win_x64.msi" -O static/builds/libreoffice_win64.msi
+wget "https://download.documentfoundation.org/libreoffice/stable/6.3.1/win/x86/LibreOffice_6.3.1_Win_x86.msi" -O static/builds/libreoffice_win32.msi
+wget "https://download.documentfoundation.org/libreoffice/stable/6.3.1/mac/x86_64/LibreOffice_6.3.1_MacOS_x86-64.dmg" -O static/builds/libreoffice_mac.dmg
 
 # Clementine
 wget "https://github.com/clementine-player/Clementine/releases/download/1.3.1/ClementineSetup-1.3.1.exe" -O static/builds/clementine_win.exe
 wget "https://github.com/clementine-player/Clementine/releases/download/1.3.1/clementine-1.3.1.dmg" -O static/builds/clementine_mac.dmg
 
 # Gimp
-wget "https://download.gimp.org/mirror/pub/gimp/v2.10/windows/gimp-2.10.8-setup-2.exe" -O static/builds/gimp_windows.exe
-wget "https://download.gimp.org/mirror/pub/gimp/v2.10/osx/gimp-2.10.8-x86_64-2.dmg" -O static/builds/gimp_mac.dmg
+wget "https://download.gimp.org/mirror/pub/gimp/v2.10/windows/gimp-2.10.12-setup-3.exe" -O static/builds/gimp_windows.exe
+wget "https://download.gimp.org/mirror/pub/gimp/v2.10/osx/gimp-2.10.12-x86_64.dmg" -O static/builds/gimp_mac.dmg
 
 # Supertux
 wget "https://github.com/SuperTux/supertux/releases/download/v0.6.0/SuperTux-v0.6.0-win64.msi" -O static/builds/supertux_win64.exe
@@ -31,12 +31,13 @@ wget "https://github.com/SuperTux/supertux/releases/download/v0.6.0/SuperTux-v0.
 wget "https://github.com/SuperTux/supertux/releases/download/v0.6.0/SuperTux-v0.6.0-Darwin.dmg" -O static/builds/supertux_mac.dmg
 
 # SuperTuxKart
-wget "https://sourceforge.net/projects/supertuxkart/files/SuperTuxKart/0.9.3/supertuxkart-0.9.3-win64.exe/download" -O static/builds/supertuxkart_win64.exe
-wget "https://sourceforge.net/projects/supertuxkart/files/SuperTuxKart/0.9.3/supertuxkart-0.9.3-win32.exe/download" -O static/builds/supertuxkart_win32.exe
-wget "https://sourceforge.net/projects/supertuxkart/files/SuperTuxKart/0.9.3/SuperTuxKart-0.9.3-osx.dmg/download" -O static/builds/supertuxkart_mac.dmg
+wget "https://sourceforge.net/projects/supertuxkart/files/SuperTuxKart/1.0/SuperTuxKart%201.0%20installer-64bit.exe/download" -O static/builds/supertuxkart_win64.exe
+wget "https://sourceforge.net/projects/supertuxkart/files/SuperTuxKart/1.0/SuperTuxKart%201.0%20installer-32bit.exe/download" -O static/builds/supertuxkart_win32.exe
+wget "https://sourceforge.net/projects/supertuxkart/files/SuperTuxKart/1.0/SuperTuxKart-1.0-mac.zip/download" -O static/builds/supertuxkart_mac.dmg
 
-#Freeciv
-wget "http://files.freeciv.org/packages/windows/Freeciv-2.6.0-win32-gtk3-setup.exe" -O static/builds/freeciv_win32.exe
+#Freecol
+wget "https://sourceforge.net/projects/freecol/files/freecol/freecol-0.11.6/freecol-0.11.6-installer.exe/download?use_mirror=netcologne&download=" -O static/builds/freecol_win32.exe
+wget "https://sourceforge.net/projects/freecol/files/freecol/freecol-0.11.6/freecol-0.11.6-mac.tar.bz2/download?use_mirror=netix&download=" -O static/builds/freecol_mac.exe
 
 
 # Difazier
